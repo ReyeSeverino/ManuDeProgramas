2c1638bf6e6846c33755cc2a24a6665e605b8648
Carlos Guillermo Reyes Severino <carlosguillermosrs@gmail.com>
2024-11-19 12:50:23 -0400

    Add project files.

---
+++ ManuDeProgramas/AppMenuDeOpciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManuDeProgramas
+{
+    public class AppMenuDeOpciones
+    {
+        public void menuCalculadora() {
+
+            Console.WriteLine(" Menu De La Calculadora");
+        }
+    }
+}
